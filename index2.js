f9cd988a6b442052b5030d90efcf75d6f9d4c0b9
Toni1993 <77364364+Toni1993@users.noreply.github.com>
2021-09-14 22:46:03 +0300

    finish

--- index2.js
+++ index2.js
@@ -1,26 +1,26 @@
-let targetDate = new Date('jan 1 2022 00:00:00')
-function counts() {
-  let now = new Date()
-  let gap = targetDate - now
-  console.log(gap)
+// let targetDate = new Date('jan 1 2022 00:00:00')
+// function counts() {
+//   let now = new Date()
+//   let gap = targetDate - now
+//   console.log(gap)
 
-  const days = pad(Math.floor(gap / (1000 * 60 * 60 * 24)))
-  const hours = pad(Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
-  const mins = pad(Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60)))
-  const secs = pad(Math.floor((gap % (1000 * 60)) / 1000))
-  console.log(days)
-  if (gap < 0) {
-    document.querySelector('[data-value="days"]').innerText = 'Ура'
-  } else {
-    document.querySelector('[data-value="days"]').innerText = days
-    document.querySelector('[data-value="hours"]').innerText = hours
-    document.querySelector('[data-value="mins"]').innerText = mins
-    document.querySelector('[data-value="secs"]').innerText = secs
-  }
-}
-counts()
-setInterval(counts, 1000)
+//   const days = pad(Math.floor(gap / (1000 * 60 * 60 * 24)))
+//   const hours = pad(Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+//   const mins = pad(Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60)))
+//   const secs = pad(Math.floor((gap % (1000 * 60)) / 1000))
+//   console.log(days)
+//   if (gap < 0) {
+//     document.querySelector('[data-value="days"]').innerText = 'Ура'
+//   } else {
+//     document.querySelector('[data-value="days"]').innerText = days
+//     document.querySelector('[data-value="hours"]').innerText = hours
+//     document.querySelector('[data-value="mins"]').innerText = mins
+//     document.querySelector('[data-value="secs"]').innerText = secs
+//   }
+// }
+// counts()
+// setInterval(counts, 1000)
 
-function pad(value) {
-  return String(value).padStart(2, '0')
-}
+// function pad(value) {
+//   return String(value).padStart(2, '0')
+// }
